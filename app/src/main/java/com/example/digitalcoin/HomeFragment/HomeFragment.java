903e3907e381f506946e76b9079b900c14b11307
Sajad <sajadzarini61@gmail.com>
2023-12-15 18:49:46 +0330

    show 10 top market coin in app

--- app/src/main/java/com/example/digitalcoin/HomeFragment/HomeFragment.java
+++ app/src/main/java/com/example/digitalcoin/HomeFragment/HomeFragment.java
@@ -3,6 +3,10 @@ package com.example.digitalcoin.HomeFragment;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,27 +20,26 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.example.digitalcoin.HomeFragment.adapter.SliderImageAdapter;
+import com.example.digitalcoin.HomeFragment.adapter.TopCoinRvAdapter;
 import com.example.digitalcoin.MainActivity;
 import com.example.digitalcoin.R;
-import com.example.digitalcoin.RoomDB.Entities.MarketListEntity;
 import com.example.digitalcoin.databinding.FragmentHomeBinding;
 import com.example.digitalcoin.models.cryptoListModel.AllMarketModel;
+import com.example.digitalcoin.models.cryptoListModel.DataItem;
 import com.example.digitalcoin.viewModels.AppViewModels;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import dagger.hilt.android.AndroidEntryPoint;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 @AndroidEntryPoint
@@ -49,6 +52,10 @@ public class HomeFragment extends Fragment {
     @Inject
     @Named("fullName")
     String name;
+
+    public List<String> top_wants = Arrays.asList("BTC", "ETH", "BNB", "ADA", "XRP", "DOGE", "DOT", "UNI", "LTC", "LINK");
+    TopCoinRvAdapter topCoinRvAdapter;
+    CompositeDisposable compositeDisposable;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -64,14 +71,13 @@ public class HomeFragment extends Fragment {
     }
 
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         fragmentHomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
-
         appViewModels = new ViewModelProvider(requireActivity()).get(AppViewModels.class);
-        
+        compositeDisposable = new CompositeDisposable();
+
         setUpViewPager2();
 
         getAllMarketDataFromDB();
@@ -81,26 +87,40 @@ public class HomeFragment extends Fragment {
 
     @SuppressLint("CheckResult")
     private void getAllMarketDataFromDB() {
-        appViewModels.getAllMarketData()
+        Disposable disposable = appViewModels.getAllMarketData()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(marketListEntity -> {
                     AllMarketModel allMarketModel = marketListEntity.getAllMarketModel();
 
-                    Log.e("TAG", "getAllMarketDataFromDB: " + allMarketModel.getData().getCryptoCurrencyList().get(0).getName() );
-                    Log.e("TAG", "getAllMarketDataFromDB: " + allMarketModel.getData().getCryptoCurrencyList().get(1).getName() );
-                });
+                    ArrayList<DataItem> top10 = new ArrayList<>();
+                    for (int i = 0; i < allMarketModel.getData().getCryptoCurrencyList().size(); i++) {
+                        for (int j = 0; j < top_wants.size(); j++) {
+                            String coin_name = top_wants.get(j);
+                            if (allMarketModel.getData().getCryptoCurrencyList().get(i).getSymbol().equals(coin_name)) {
+                                DataItem dataItem = allMarketModel.getData().getCryptoCurrencyList().get(i);
+                                top10.add(dataItem);
+                            }
+                        }
+                    }
 
+                    if (fragmentHomeBinding.topCoinRv.getAdapter() != null) {
+                        topCoinRvAdapter = (TopCoinRvAdapter) fragmentHomeBinding.topCoinRv.getAdapter();
+                        topCoinRvAdapter.updateData(top10);
+                    } else {
+                        topCoinRvAdapter = new TopCoinRvAdapter(top10);
+                        fragmentHomeBinding.topCoinRv.setAdapter(topCoinRvAdapter);
+                    }
+
+                });
+        compositeDisposable.add(disposable);
     }
 
     private void setUpViewPager2() {
-        appViewModels.getMutableLiveData().observe(getActivity(), new Observer<ArrayList<Integer>>() {
-            @Override
-            public void onChanged(ArrayList<Integer> arrayList) {
-                Log.e("test", arrayList.toString());
-                fragmentHomeBinding.viewPagerImageSlider.setAdapter(new SliderImageAdapter(arrayList));
-                fragmentHomeBinding.viewPagerImageSlider.setOffscreenPageLimit(3);
-            }
+        appViewModels.getMutableLiveData().observe(getActivity(), arrayList -> {
+            Log.e("test", arrayList.toString());
+            fragmentHomeBinding.viewPagerImageSlider.setAdapter(new SliderImageAdapter(arrayList));
+            fragmentHomeBinding.viewPagerImageSlider.setOffscreenPageLimit(3);
         });
     }
 
