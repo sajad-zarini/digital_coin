682493dfe6d30c2f2bed3a32baac35546b7c5f68
Sajad <sajadzarini61@gmail.com>
2023-12-22 20:10:08 +0330

    complete PrivacyPolicyFragment.java section in project

--- app/src/main/java/com/example/digitalcoin/UI/OtherFragment/PrivacyPolicyFragment.java
+++ app/src/main/java/com/example/digitalcoin/UI/OtherFragment/PrivacyPolicyFragment.java
@@ -1,66 +1,86 @@
 package com.example.digitalcoin.UI.OtherFragment;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.digitalcoin.R;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PrivacyPolicyFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.digitalcoin.MainActivity;
+import com.example.digitalcoin.R;
+import com.example.digitalcoin.databinding.FragmentPrivacyPolicyBinding;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import me.ibrahimsn.lib.SmoothBottomBar;
+
 public class PrivacyPolicyFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    FragmentPrivacyPolicyBinding fragmentPrivacyPolicyBinding;
+    SmoothBottomBar bottomNavigationBar;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    MainActivity mainActivity;
 
-    public PrivacyPolicyFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PrivacyPolicyFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PrivacyPolicyFragment newInstance(String param1, String param2) {
-        PrivacyPolicyFragment fragment = new PrivacyPolicyFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@androidx.annotation.NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        fragmentPrivacyPolicyBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_privacy_policy,container,false);
+        HideBottomNavigationBar();
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_privacy_policy, container, false);
+        return fragmentPrivacyPolicyBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@androidx.annotation.NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setupToolbar(view);
+    }
+
+    private void setupToolbar(View view) {
+        NavController navController = Navigation.findNavController(view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.privacy_policy_fragment)
+                .build();
+        Toolbar toolbar = view.findViewById(R.id.empty_toolbar);
+
+
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.privacy_policy_fragment){
+                toolbar.setNavigationIcon(R.drawable.baseline_arrow_back_24);
+                toolbar.setTitle(R.string.privacy_policy);
+            }
+        });
+    }
+
+    @Override
+    public void onAttach(@androidx.annotation.NonNull @NonNull Context context) {
+        super.onAttach(context);
+        mainActivity = (MainActivity) context;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        requireActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        bottomNavigationBar.setVisibility(View.VISIBLE);
+    }
+
+
+    private void HideBottomNavigationBar() {
+        bottomNavigationBar = requireActivity().findViewById(R.id.bottom_navigation);
+        bottomNavigationBar.setVisibility(View.GONE);
     }
 }
