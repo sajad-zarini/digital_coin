68613978d9af65bb94797b60b9966180f3a97b41
Sajad <sajadzarini61@gmail.com>
2023-12-22 23:46:44 +0330

    complete show detail of coin in DetailFragment.java without firebase analytics and data binding change time

---
+++ app/src/main/java/com/example/digitalcoin/UI/DetailFragment/DetailRvAdapter.java
@@ -0,0 +1,84 @@
+package com.example.digitalcoin.UI.DetailFragment;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.digitalcoin.R;
+import com.example.digitalcoin.databinding.DetailFragmentRvItemsBinding;
+
+import java.util.ArrayList;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+
+public class DetailRvAdapter extends RecyclerView.Adapter<DetailRvAdapter.DetailRvHolder> {
+
+    ArrayList<String> keys;
+    ArrayList<String> values;
+    LayoutInflater layoutInflater;
+
+    public DetailRvAdapter(ArrayList<String> keys, ArrayList<String> values) {
+        this.keys = keys;
+        this.values = values;
+    }
+
+    @androidx.annotation.NonNull
+    @NonNull
+    @Override
+    public DetailRvHolder onCreateViewHolder(@androidx.annotation.NonNull @NonNull ViewGroup parent, int viewType) {
+        if (layoutInflater == null) {
+            layoutInflater = LayoutInflater.from(parent.getContext());
+        }
+        DetailFragmentRvItemsBinding detailFragmentRvItemsBinding = DataBindingUtil.inflate(layoutInflater, R.layout.detail_fragment_rv_items, parent, false);
+        return new DetailRvHolder(detailFragmentRvItemsBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DetailRvHolder holder, int position) {
+        holder.bind(keys.get(position), values.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return keys.size();
+    }
+
+    static class DetailRvHolder extends RecyclerView.ViewHolder {
+
+        DetailFragmentRvItemsBinding detailFragmentRvItemsBinding;
+
+        public DetailRvHolder(@NonNull DetailFragmentRvItemsBinding detailFragmentRvItemsBinding) {
+            super(detailFragmentRvItemsBinding.getRoot());
+            this.detailFragmentRvItemsBinding = detailFragmentRvItemsBinding;
+        }
+
+        @SuppressLint("SetTextI18n")
+        public void bind(String key, String value) {
+            if (key.equals("PercentChange 7d") || key.equals("PercentChange 30d")) {
+                setColorText(value, detailFragmentRvItemsBinding.detailValues);
+                detailFragmentRvItemsBinding.detailValues.setText(value + "%");
+            } else {
+                detailFragmentRvItemsBinding.detailValues.setText(value);
+            }
+            detailFragmentRvItemsBinding.detailKeys.setText(key);
+            detailFragmentRvItemsBinding.executePendingBindings();
+        }
+
+        private void setColorText(String value, TextView detailValues) {
+            if (Float.parseFloat(value) < 0) {
+                detailValues.setTextColor(Color.RED);
+            } else if (Float.parseFloat(value) > 0) {
+                detailValues.setTextColor(Color.GREEN);
+            } else {
+                detailValues.setTextColor(Color.WHITE);
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/digitalcoin/UI/DetailFragment/LandScapeChartFragment.java
@@ -0,0 +1,69 @@
+package com.example.digitalcoin.UI.DetailFragment;
+
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.digitalcoin.Models.cryptoListModel.DataItem;
+import com.example.digitalcoin.R;
+import com.example.digitalcoin.databinding.FragmentLandScapeChartBinding;
+
+import me.ibrahimsn.lib.SmoothBottomBar;
+
+public class LandScapeChartFragment extends Fragment {
+
+    SmoothBottomBar bottomNavigationBar;
+    FragmentLandScapeChartBinding fragmentLandScapeChartBinding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        requireActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        fragmentLandScapeChartBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_land_scape_chart, container, false);
+
+        // get Data from Bundle
+        DataItem dataItem = getArguments() != null ? getArguments().getParcelable("model") : null;
+
+
+        HideBottomNavigationBar();
+
+        if (dataItem != null) {
+            setupChart(dataItem);
+        }
+
+        // Inflate the layout for this fragment
+        return fragmentLandScapeChartBinding.getRoot();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        requireActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        bottomNavigationBar.setVisibility(View.VISIBLE);
+    }
+
+    private void HideBottomNavigationBar() {
+        bottomNavigationBar = requireActivity().findViewById(R.id.bottom_navigation);
+        bottomNavigationBar.setVisibility(View.GONE);
+    }
+
+    // setup Chart (WebView) for first Time
+    @SuppressLint("SetJavaScriptEnabled")
+    private void setupChart(DataItem dataItem) {
+        fragmentLandScapeChartBinding.chartWebView.getSettings().setJavaScriptEnabled(true);
+        fragmentLandScapeChartBinding.chartWebView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        fragmentLandScapeChartBinding.chartWebView.loadUrl("https://s.tradingview.com/widgetembed/?frameElementId=tradingview_76d87&symbol=" + dataItem.getSymbol() + "USD&interval=D&hidesidetoolbar=1&symboledit=1&saveimage=1&toolbarbg=F1F3F6&studies=[]&hideideas=1&theme=Dark&style=1&timezone=Etc%2FUTC&studies_overrides={}&overrides={}&enabled_features=[]&disabled_features=[]&locale=en&utm_source=coinmarketcap.com&utm_medium=widget&utm_campaign=chart&utm_term=BTCUSDT");
+
+    }
+
+}
